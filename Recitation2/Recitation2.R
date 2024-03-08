9dcb19847c80085db066453d00dfce7673e8f972
bugragncl <bgrgncl@gmail.com>
2024-03-08 15:31:36 +0300

    Recitation2

--- Recitation2/Recitation2.R
+++ Recitation2/Recitation2.R
@@ -58,3 +58,126 @@ load("myvar.RData")
 #lastly check the objects in the global environment.
 ls()
 
+
+
+
+# Function using for loop
+sum_of_squares_loop <- function(x) {
+  sum_squares <- 0
+  for (i in 1:length(x)) {
+    sum_squares <- sum_squares + x[i]^2
+  }
+  return(sum_squares)
+}
+
+# Function without using for loop
+sum_of_squares_no_loop <- function(x) {
+  sum_squares <- sum(x^2)
+  return(sum_squares)
+}
+
+# Generate a vector for testing
+large_vector <- rnorm(10^6)
+
+# Test the two different function
+time_loop <- system.time(sum_of_squares_loop(large_vector))
+time_no_loop <- system.time(sum_of_squares_no_loop(large_vector))
+
+#results
+cat("Execution time using for loop:", time_loop[3], "seconds\n")
+
+cat("Execution time without using a loop:", time_no_loop[3], "seconds\n")
+
+
+data <- read.csv("https://users.ssc.wisc.edu/~hemken/Rworkshops/read/classm.csv")
+
+lapply <- system.time({
+  fill_na_with_mean <- function(fill_column) {
+    mean_val <- mean(fill_column, na.rm = TRUE)
+    fill_column[is.na(fill_column)] <- mean_val
+    return(fill_column)
+  }
+  #using lapply
+  lapply(data, fill_na_with_mean)
+})
+##
+loop <- system.time({
+  fill_na_with_mean <- function(fill_column) {
+    mean_val <- mean(fill_column, na.rm = TRUE)
+    fill_column[is.na(fill_column)] <- mean_val
+    return(fill_column)
+  }
+  
+  #using loops
+  for (col in names(data)) {
+    data[[col]] <- fill_na_with_mean(data[[col]])
+  }
+})
+cat("Execution time using lappy:", lapply[3], "seconds\n")
+
+cat("Execution time using a loop:", loop[3], "seconds\n")
+
+
+
+daily_activity <- function() {
+  activities <- c("Waking up", "Breakfast", "Work", "Lunch break", "Afternoon work", "Dinner", "Relaxation", "Bedtime")
+  durations <- c(5, 10, 20, 5, 10, 5, 10, 5)  # Duration of each activity as seconds
+  cat("Simulating daily activities...\n")
+  
+  for (i in seq_along(activities)) {
+    cat("Activities:", activities[i], "...\n")
+    Sys.sleep(durations[i])
+  }
+  cat("Day complete. Time to rest.\n")
+}
+daily_activity()
+
+
+library(magrittr)
+# Apply operations using pipe operator
+seq(-0, 100, 4) |> 
+  divide_by(2) |> 
+  add(10) |> 
+  log() |> 
+  round(digits = 2) |> 
+  head(10)
+
+matrix_q3 <- matrix(c(rnorm(4),rnorm(4),rnorm(4), rnorm(4)), nrow=4, byrow=TRUE)
+matrix_q3 %>% solve()  %>%  '%*%' (matrix_q3) %>% round(10)
+
+data <- read.csv("https://users.ssc.wisc.edu/~hemken/Rworkshops/read/classm.csv")
+data %>%
+  subset(Height > 56.5) %>% 
+  aggregate(Height ~ Sex, ., mean) 
+
+female <- subset(data, Sex == "F") %>% 
+  summarise(Mean_Height = mean(Height),
+            Median_Height = median(Height),
+            Min_Height = min(Height),
+            Max_Height = max(Height)) 
+
+male <- subset(data, Sex == "M") %>% na.omit() %>% #there are some NA values in male
+  summarise(Mean_Height = mean(Height),
+            Median_Height = median(Height),
+            Min_Height = min(Height),
+            Max_Height = max(Height))
+
+female$Sex <- "F"
+male$Sex <- "M"
+results <- bind_rows(female, male)
+results
+library(dplyr)
+student <- read.csv("https://raw.githubusercontent.com/shriyaa01/Student-Result-Analysis/main/Expanded_data_with_more_features.csv", sep = ",")
+head(student)
+
+install.packages("dplyr")
+library(dplyr)
+new_student <- student |> select(PracticeSport, MathScore, ReadingScore, IsFirstChild,  LunchType)
+new_student |> head(10)
+
+q4_c <- new_student |> na.omit()
+#Check NA values
+sum(is.na(q4_c))
+
+#Check the Structure
+str(q4_c)
